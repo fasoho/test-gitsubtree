139bb6fca00a9fe3b1c2a829fab4ec0ed02c8bc1
JP <1.4.gigz@gmail.com>
2019-02-13 21:09:29 -0600

    Update View to show Hello World message. Closes #6.

--- python-exercises/django/hello_world/pages/views.py
+++ python-exercises/django/hello_world/pages/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import textwrap
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic.base import View
+
+class HomePageView(View):
+  def dispatch(request, *args, **kwargs):
+    response_text = textwrap.dedent('''\
+      <!DOCTYPE html>
+      <head>
+        <title>Hello World</title>
+      </head>
+      <body>
+        <h2>Hello World</h2>
+      </body>
+      </html>
+      
+    ''')
+    return HttpResponse(response_text)
