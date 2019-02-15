e0b11edd102dfc1e11a95c81bc20bcf8d982c9ba
JP <1.4.gigz@gmail.com>
2019-02-15 17:16:13 -0600

    Set up HelloController. Closes #15.

---
+++ java-exercises/spring/HelloWorld/src/main/java/com/fasoho/helloworld/controllers/HelloController.java
@@ -0,0 +1,23 @@
+package com.fasoho.helloworld.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @project HelloWorld
+ * @user jp
+ * @date 2019-02-15
+ */
+@Controller
+public class HelloController {
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello";
+  }
+
+}
